2f914850e639a8858cb5634eab16113645dc26ad
srrobin <srrobin07@gmail.com>
2022-12-26 14:57:43 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SimpleNavbar from './layouts/NavbarLayouts/SimpleNavbar';
+
+
+const App = () => {
+  return (
+    <div className='bg-white dark:bg-black'>
+       <SimpleNavbar/>
+    </div>
+  );
+};
+
+export default App;
